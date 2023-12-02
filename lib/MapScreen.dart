da4370941f4501c8be750aff83dbd1ba4dad834f
LexaProInvoke <leha.tkachenko.05@gmail.com>
2023-12-03 00:26:25 +0100

    temp

--- lib/MapScreen.dart
+++ lib/MapScreen.dart
@@ -1,112 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({Key? key}) : super(key: key);
-
   @override
-  State<MapScreen> createState() => _MapScreenState();
+  _MapScreenState createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
-  late final MapController _mapController;
+  late Position _currentPosition;
 
   @override
   void initState() {
-    _mapController = MapController();
     super.initState();
+    _getLocation();
   }
 
-  @override
-  void dispose() {
-    _mapController.dispose();
-    super.dispose();
+  Future<void> _getLocation() async {
+    try {
+      _currentPosition = await _determinePosition();
+      setState(() {});
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw 'Location services are disabled.';
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw 'Location permissions are denied.';
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw 'Location permissions are permanently denied, we cannot request permissions.';
+    }
+
+    return await Geolocator.getCurrentPosition();
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<LatLng> _mapPoints = [
-      LatLng(55.755793, 37.617134),
-      LatLng(55.095960, 38.765519),
-      LatLng(56.129038, 40.406502),
-      LatLng(54.513645, 36.261268),
-      LatLng(54.193122, 37.617177),
-      LatLng(54.629540, 39.741809),
-    ];
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Map Screen'),
+        title: Text('Map Screen'),
       ),
-      body: FlutterMap(
-        mapController: _mapController,
-        options: MapOptions(
-          center: _mapPoints[0],
-          zoom: 5,
-        ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.flutter_map_example',
-          ),
-          MarkerClusterLayerWidget(
-            options: MarkerClusterLayerOptions(
-              size: const Size(50, 50),
-              maxClusterRadius: 50,
-              markers: _getMarkers(_mapPoints),
-              builder: (_, markers) {
-                return _ClusterMarker(
-                  markersLength: markers.length.toString(),
-                );
-              },
+      body: Center(
+        child: _currentPosition != null
+            ? Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Latitude: ${_currentPosition.latitude}',
+              style: TextStyle(fontSize: 20.0),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-List<Marker> _getMarkers(List<LatLng> mapPoints) {
-  return mapPoints
-      .map(
-        (point) => Marker(
-      point: point,
-      width: 50,
-      height: 50,
-      builder: (_) => Image.asset('assets/icons/map_point.png'),
-    ),
-  )
-      .toList();
-}
-
-class _ClusterMarker extends StatelessWidget {
-  const _ClusterMarker({required this.markersLength});
-
-  final String markersLength;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.blue[200],
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: Colors.blue,
-          width: 3,
-        ),
-      ),
-      child: Center(
-        child: Text(
-          markersLength,
-          style: const TextStyle(
-            color: Colors.blue,
-            fontWeight: FontWeight.w700,
-            fontSize: 18,
-          ),
-        ),
+            Text(
+              'Longitude: ${_currentPosition.longitude}',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ],
+        )
+            : CircularProgressIndicator(),
       ),
     );
   }
