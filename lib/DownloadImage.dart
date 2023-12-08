1a1ba55bc570e25b0c8d95cde821ffa553f3f9b2
LexaProInvoke <leha.tkachenko.05@gmail.com>
2023-12-08 18:53:44 +0100

    add imageName

--- lib/DownloadImage.dart
+++ lib/DownloadImage.dart
@@ -4,8 +4,11 @@ import 'package:http/http.dart' as http;
 class DownloadImage {
   final String baseUrl = 'http://192.168.137.216:8080'; // Замените на ваш IP-адрес
 
-  Future<List<int>> fetchImageByNumber(int imageNumber) async {
-    final response = await http.get(Uri.parse('$baseUrl/images/$imageNumber'));
+  Future<List<int>> fetchImageByNumber(int imageNumber, String fileName) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/images'),
+      headers: {'ficheId' : imageNumber.toString(), 'fileName': fileName}, // Добавление переменной fileName в заголовок
+    );
 
     if (response.statusCode == 200) {
       return response.bodyBytes;
